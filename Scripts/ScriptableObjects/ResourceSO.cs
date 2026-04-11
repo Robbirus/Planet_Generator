ca4461cc1a8fb989e593577640272dd687d15f0b
Clément Faivre <156783292+Robbirus@users.noreply.github.com>
2026-04-11 17:13:22 +0200

    :fire: delete Scripts directory

--- Scripts/ScriptableObjects/ResourceSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ResourceSO", menuName = "Game/Celestials/Resource Data")]
-public class ResourceSO : ScriptableObject
-{
-    [Tooltip("Pool of resources that can appear on this planet type. The percentages are randomised at runtime.")]
-    public List<ResourceType> availableResources = new();
-}
