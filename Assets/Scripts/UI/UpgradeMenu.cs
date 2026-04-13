1ff89c2b2ff007ba8a7d065ae62aa3e3820ae810
faivr <faivr@CLEMENT-DESKTOP>
2026-04-13 21:36:53 +0200

    :lipstick: added the UI

--- Assets/Scripts/UI/UpgradeMenu.cs
+++ Assets/Scripts/UI/UpgradeMenu.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class UpgradeMenu : MonoBehaviour
 {
+    [Header("Inventory")]
+    [SerializeField] private ShipInventory inventory;
+    [Space(5)]
+
+    [Header("UI References")]
     [SerializeField] private TMP_Text resourceList;
+
+    private Dictionary<ResourceType, float> stock = new Dictionary<ResourceType, float>();
 
     public void SetCurrentUpgrade()
     {
@@ -13,5 +21,19 @@ public class UpgradeMenu : MonoBehaviour
     public void PurchaseUpgrade()
     {
         Debug.Log("Skill purchased");
+    }
+
+    public void Update()
+    {
+        if (inventory != null && gameObject.activeSelf == true)
+        {
+            stock = inventory.GetAll();
+            resourceList.text = "Resource :\n";
+
+            foreach(ResourceType key in stock.Keys)
+            {
+                resourceList.text += key.ToString() + " : " + inventory.Get(key).ToString("0") + "\n";
+            }
+        }
     }
 }
