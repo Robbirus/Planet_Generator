a9ce12d362a46f04c325c01763ec66dbd4ad0f29
Leïla P. <leila.karukera@gmail.com>
2026-04-17 11:54:07 +0200

    ✨ Update NoiseFilter

--- Assets/Scripts/PlanetTerrain/NoiseFilter.cs
+++ Assets/Scripts/PlanetTerrain/NoiseFilter.cs
@@ -14,7 +14,19 @@ public class NoiseFilter
     }
     public float Evaluate(Vector3 point)
     {
-        float noiseValue = (noise.Evaluate(point * settings.roughness + settings.centre) + 1) * .5f;
+        float noiseValue = 0;
+        float frequency = settings.baseRoughness;
+        float amplitude = 1;
+
+        for (int i = 0; i < settings.numLayers; i++) 
+        {
+            float value = noise.Evaluate(point * frequency + settings.centre);
+            noiseValue += (value + 1) * .5f * amplitude;
+            frequency *= settings.roughness;
+            amplitude *= settings.persistence;
+        }
+
+        noiseValue = Mathf.Max(0, noiseValue - settings.minValue);
         return noiseValue * settings.strength;
     }
 }
