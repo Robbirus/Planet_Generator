a9df331e97eeaf19a850bf9849293d25f06d9785
Leïla P. <leila.karukera@gmail.com>
2026-04-17 11:53:46 +0200

    ✨ Update NoiseSettings

--- Assets/Scripts/PlanetTerrain/NoiseSettings.cs
+++ Assets/Scripts/PlanetTerrain/NoiseSettings.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class NoiseSettings
 {
     public float strength = 1;
-    public float roughness = 1;
+    [Range(1,8)]
+    public int numLayers = 1; // number of layers of noise
+    public float baseRoughness = 1;
+    public float roughness = 2;
+    public float persistence = .5f; // controls how fast the amplitude of the noise decreases with each layer
     public Vector3 centre;
+    public float minValue; // gets the terrain to recede into the base sphere of the planet
 }
