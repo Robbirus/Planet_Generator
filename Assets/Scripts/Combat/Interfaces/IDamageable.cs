3e1c9177a081164150c07d058e6e9fdd7dd0fade
faivr <faivr@CLEMENT-DESKTOP>
2026-04-20 01:01:40 +0200

    :sparkles: added effect difference

--- Assets/Scripts/Combat/Interfaces/IDamageable.cs
+++ Assets/Scripts/Combat/Interfaces/IDamageable.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 public interface IDamageable
 {
-    void TakeDamage(float damage, bool isCrit);   
+    void TakeDamage(float damage, Color color, bool isEffect, bool isCrit);   
     void HandleHit(Shell shell, RaycastHit hit);
 }
