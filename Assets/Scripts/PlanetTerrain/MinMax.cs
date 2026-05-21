033dd285cc6cc453ebb3cd3c7018dbca19736e6c
Leïla P. <leila.karukera@gmail.com>
2026-05-21 16:47:36 +0200

    ✨ Add MinMax script to keep track of min and max elevation values

---
+++ Assets/Scripts/PlanetTerrain/MinMax.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+// Class to handle min and max elevation values on planet landmasses
+public class MinMax 
+{
+    public float Min { get; private set; }
+    public float Max { get; private set; }
+
+    public MinMax()
+    {
+        Min = float.MaxValue;
+        Max = float.MinValue;
+    }
+
+    public void AddValue(float value)
+    { 
+        if (value > Max)
+        {
+            Max = value;
+        }
+
+        if (value < Min)
+        {
+            Min = value;
+        }
+    }
+
+}
